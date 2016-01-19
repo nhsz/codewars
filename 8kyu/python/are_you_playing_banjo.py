b8fa6ccb79625e46eb6b74141669e14034bd97e1
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-19 03:49:53 -0300

    Add Are You Playing Banjo? Kata

---
+++ 8kyu/python/are_you_playing_banjo.py
@@ -0,0 +1,8 @@
+# http://www.codewars.com/kata/53af2b8861023f1d88000832/
+
+
+def are_you_playing_banjo(name):
+    if name[0].lower() == "r":
+        return(name + " plays banjo")
+    else:
+        return(name + " does not play banjo")
