1aea9199a3c41b9aee65d4bda8250021e1ebfbaa
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-23 15:32:26 -0300

    Add Classy Extentions Kata

---
+++ 8kyu/python/classy_extentions.py
@@ -0,0 +1,6 @@
+# http://www.codewars.com/kata/55a14aa4817efe41c20000bc/
+
+
+class Cat(Animal):
+    def speak(self):
+        return self.name + " meows."
--- 8kyu/python/online_rpg_player_to_qualifying_stage.py
+++ 8kyu/python/online_rpg_player_to_qualifying_stage.py
@@ -2,7 +2,4 @@
 
 
 def player_rank_up(pts):
-    if pts >= 100:
-         return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
-
-    return False
+    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." if pts >= 100 else False
