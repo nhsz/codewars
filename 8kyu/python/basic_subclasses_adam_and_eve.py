9fec53993af58509e4b86688163c336fad59bc2c
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-23 20:05:22 -0300

    Add Basic subclasses - Adam and Eve Kata

---
+++ 8kyu/python/basic_subclasses_adam_and_eve.py
@@ -0,0 +1,17 @@
+# http://www.codewars.com/kata/547274e24481cfc469000416/
+
+
+def God():
+    return [Man(), Woman()]
+
+
+class Human(object):
+    pass
+
+
+class Man(Human):
+    pass
+
+
+class Woman(Human):
+    pass
