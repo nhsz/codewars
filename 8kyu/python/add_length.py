44e1be914e1dcf82dc38ea42d72d6c3255646637
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-23 18:07:15 -0300

    Add Add Length Kata

---
+++ 8kyu/python/add_length.py
@@ -0,0 +1,12 @@
+# http://www.codewars.com/kata/559d2284b5bb6799e9000047/
+
+
+def add_length(str_):
+    list = str_.split()
+    new_list = []
+
+    for word in list:
+        new_word = word + " " + str(len(word))
+        new_list.append(new_word)
+
+    return new_list
