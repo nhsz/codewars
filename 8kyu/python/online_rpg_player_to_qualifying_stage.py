f1264f3e480faa99d5361fe1bd90acba5d72bc6d
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-23 15:21:56 -0300

    Add Online RPG: player to qualifying stage? Kata

---
+++ 8kyu/python/online_rpg_player_to_qualifying_stage.py
@@ -0,0 +1,8 @@
+# http://www.codewars.com/kata/55849d76acd73f6cc4000087/
+
+
+def player_rank_up(pts):
+    if pts >= 100:
+         return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up."
+
+    return False
