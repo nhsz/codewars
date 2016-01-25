bc0fc331c9cd7f2ce97e73ae1c7ea4cc3c13e8d2
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-25 12:43:43 -0300

    Add Welcome to the City Kata

---
+++ 8kyu/python/welcome_to_the_city.py
@@ -0,0 +1,6 @@
+# http://www.codewars.com/kata/5302d846be2a9189af0001e4/
+
+
+def say_hello(name, city, state):
+    name = " ".join(name)
+    return "Hello, {}! Welcome to {}, {}!".format(name, city, state)
