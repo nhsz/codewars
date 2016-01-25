66a1eebcd3c1a40ef40ce11227e4df3d7fba195f
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-01-25 13:29:35 -0300

    Add Grasshopper - Debug Kata

---
+++ 8kyu/python/grasshopper_debug.py
@@ -0,0 +1,14 @@
+# http://www.codewars.com/kata/55cb854deb36f11f130000e1/
+
+
+def weather_info(temp):
+    celsius = convert_to_celsius(temp)
+
+    if (celsius <= 0):
+        return (str(celsius) + " is freezing temperature")
+    else:
+        return (str(celsius) + " is above freezing temperature")
+
+
+def convert_to_celsius(fahrenheit):
+  return (fahrenheit - 32) * (5 / 9.)
