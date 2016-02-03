e4d45ea223542a502a95199ba428b3149bd68383
Nicolás Quiroz <nquiroz@dc.uba.ar>
2016-02-03 12:44:07 -0300

    Add Count by X Kata

---
+++ 8kyu/python/count_by_x.py
@@ -0,0 +1,9 @@
+# http://www.codewars.com/kata/5513795bd3fafb56c200049e/
+
+
+def count_by(x, n):
+    """
+    Return a sequence of numbers counting by `x` `n` times.
+    """
+    return [x * i for i in xrange(1, n + 1)]
+
