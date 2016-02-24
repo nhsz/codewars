28cc742c5b1e022159a2f46bc3cebb9c052d8c27
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-24 16:31:04 -0300

    Add how_do_i_compare_numbers.py Kata

---
+++ 8kyu/python/how_do_i_compare_numbers.py
@@ -0,0 +1,10 @@
+# http://www.codewars.com/kata/55d8618adfda93c89600012e/
+
+
+def what_is(x):
+    if x == 42:
+        return 'everything'
+    elif x == 42 * 42:
+        return 'everything squared'
+    else:
+        return 'nothing'
