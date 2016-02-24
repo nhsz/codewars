c6cf1c137a55126e1b8f2a83a19c69966e48afdc
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-24 18:35:54 -0300

    Add color_ghost.py Kata

---
+++ 8kyu/python/color_ghost.py
@@ -0,0 +1,13 @@
+# http://www.codewars.com/kata/53f1015fa9fe02cbda00111a/
+
+
+import random
+
+
+def get_color():
+    return random.choice(["white", "yellow", "purple", "red"])
+        
+        
+class Ghost(object):
+    def __init__(self):
+        self.color = get_color()
