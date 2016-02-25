e68165ba49ef13f78d738b7ac64e027f6e3444ae
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:26:15 -0300

    Delete jennys_secret_message.py

--- 8kyu/python/jennys_secret_message.py
+++
@@ -1,9 +0,0 @@
-# http://www.codewars.com/kata/55225023e1be1ec8bc000390/
-
-
-def greet(name):
-    if name == "Johnny":
-        return "Hello, my love!"
-
-    return "Hello, {name}!".format(name=name)
-
