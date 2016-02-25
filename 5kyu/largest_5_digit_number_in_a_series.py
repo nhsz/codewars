cbded8d5ee2d86646d2e42654b9f5956d28725b2
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 01:23:17 -0300

    Create largest_5_digit_number_in_a_series.py

---
+++ 5kyu/largest_5_digit_number_in_a_series.py
@@ -0,0 +1,14 @@
+# http://www.codewars.com/kata/51675d17e0c1bed195000001/
+
+def solution(digits):
+    start = 0
+    end = 4
+    largest_number_found = digits[start:end + 1]
+    
+    while end < len(digits):
+        current_number = digits[start:end + 1]
+        if current_number > largest_number_found:
+            largest_number_found = current_number
+        start += 1 
+        end += 1 
+    return int(largest_number_found)
