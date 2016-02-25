59b4bf120c8e6be71594c7cb0b344b75a54d81fd
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:32:27 -0300

    Delete wilson_primes.py

--- 8kyu/python/wilson_primes.py
+++
@@ -1,8 +0,0 @@
-# http://www.codewars.com/kata/55dc4520094bbaf50e0000cb/
-
-
-from math import factorial
-
-
-def am_i_wilson(n):
-    return (n > 1 and n <= 563 and (factorial(n - 1) + 1) % (n * n) == 0)
