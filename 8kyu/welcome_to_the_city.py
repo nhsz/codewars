86659474fff5e4a4e961b1c5c5610392b2fd32a7
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 01:41:50 -0300

    Update welcome_to_the_city.py

--- 8kyu/welcome_to_the_city.py
+++ 8kyu/welcome_to_the_city.py
@@ -1,6 +1,5 @@
 # http://www.codewars.com/kata/5302d846be2a9189af0001e4/
 
-
 def say_hello(name, city, state):
     name = " ".join(name)
     return "Hello, {}! Welcome to {}, {}!".format(name, city, state)
