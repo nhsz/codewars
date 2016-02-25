6dfc6a8be7323ce3c56bf9393755c3c829f3fa6a
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:35:13 -0300

    Update character_frequency.py

--- 8kyu/character_frequency.py
+++ 8kyu/character_frequency.py
@@ -1,5 +1,4 @@
 # http://www.codewars.com/kata/548ef5b7f33a646ea50000b2/
 
-
 def char_freq(message):
-    return { character : message.count(character) for character in message }
+    return {character : message.count(character) for character in message}
