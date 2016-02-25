670d5ea05323d47956e847f673095c52ef6840c5
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:24:00 -0300

    Delete grasshopper_array_mean.py

--- 8kyu/python/grasshopper_array_mean.py
+++
@@ -1,8 +0,0 @@
-# http://www.codewars.com/kata/55d277882e139d0b6000005d/
-
-
-def find_average(nums):
-    try:
-        return float(sum(nums)) / len(nums)
-    except ZeroDivisionError:
-        return 0
