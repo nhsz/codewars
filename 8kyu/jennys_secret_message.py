bee4510a8e2cb692036ba35faa5a0addb1e17fb0
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 01:19:18 -0300

    Update jennys_secret_message.py

--- 8kyu/jennys_secret_message.py
+++ 8kyu/jennys_secret_message.py
@@ -1,9 +1,6 @@
 # http://www.codewars.com/kata/55225023e1be1ec8bc000390/
 
-
 def greet(name):
     if name == "Johnny":
         return "Hello, my love!"
-
     return "Hello, {name}!".format(name=name)
-
