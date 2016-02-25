52ebeedaa3358ddf224f79a418cbbb5651c0464b
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:27:05 -0300

    Delete name_shuffler.py

--- 8kyu/python/name_shuffler.py
+++
@@ -1,9 +0,0 @@
-# http://www.codewars.com/kata/559ac78160f0be07c200005a/
-
-
-def name_shuffler(str_):
-    names_list = str_.split()
-    for name in names_list:
-        swap_name = names_list[1] + " " + names_list[0]
-
-    return swap_name
