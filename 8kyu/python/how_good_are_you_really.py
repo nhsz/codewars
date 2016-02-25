d7eefabeabbc407dab5d97645d99c0f146818a4e
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:26:00 -0300

    Delete how_good_are_you_really.py

--- 8kyu/python/how_good_are_you_really.py
+++
@@ -1,6 +0,0 @@
-# http://www.codewars.com/kata/5601409514fc93442500010b/
-
-
-def better_than_average(class_points, your_points):
-    class_points.append(your_points)
-    return your_points > sum(class_points) / len(class_points)
