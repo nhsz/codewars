6e548dac923ede7e99741d7f41964c51f7070a18
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:30:36 -0300

    Delete strive_matching_1.py

--- 8kyu/python/strive_matching_1.py
+++
@@ -1,8 +0,0 @@
-# http://www.codewars.com/kata/56c22c5ae8b139416c00175d/
-
-
-def match(candidate, job):
-    try:
-        return candidate["min_salary"] * 0.9 <= job["max_salary"]
-    except: 
-        raise Exception("Invalid match")
