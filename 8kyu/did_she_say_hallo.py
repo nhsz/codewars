77eb41044d28250e2860f17c142c0e325ffa3eb3
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:39:08 -0300

    Update did_she_say_hallo.py

--- 8kyu/did_she_say_hallo.py
+++ 8kyu/did_she_say_hallo.py
@@ -1,5 +1,4 @@
 # http://www.codewars.com/kata/56a4addbfd4a55694100001f/
 
-
 def validate_hello(greetings):
     return any(x in greetings.lower() for x in ['hello','ciao','salut','hallo','hola','ahoj','czesc'])
