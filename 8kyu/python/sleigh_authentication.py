dc51caee44f78069f573ae6cd6977802fb36c16c
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:30:04 -0300

    Delete sleigh_authentication.py

--- 8kyu/python/sleigh_authentication.py
+++
@@ -1,6 +0,0 @@
-# http://www.codewars.com/kata/52adc142b2651f25a8000643/
-
-
-class Sleigh(object):
-    def authenticate(self, name, password):
-        return name == "Santa Claus" and password == "Ho Ho Ho!"
