ca6775d9d59cadd2b9c2243266976d2deba45844
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 01:58:40 -0300

    Update anagram_detection.py

--- 7kyu/anagram_detection.py
+++ 7kyu/anagram_detection.py
@@ -1,11 +1,4 @@
 # http://www.codewars.com/kata/529eef7a9194e0cbc1000255/
 
-def all_letters_contained(first_word, second_word):
-    is_contained = True
-    for letter in first_word:
-        is_contained = is_contained and letter.lower() in second_word.lower()
-    return is_contained
-
-
 def is_anagram(test, original):
-    return all_letters_contained(test, original) and all_letters_contained(original, test)
+    return sorted(original.lower()) == sorted(test.lower()) 
