ad4b7c8d62a64e585138cfd90b2bf43200037350
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:27:56 -0300

    Delete online_rpg_player_to_qualifying_stage.py

--- 8kyu/python/online_rpg_player_to_qualifying_stage.py
+++
@@ -1,5 +0,0 @@
-# http://www.codewars.com/kata/55849d76acd73f6cc4000087/
-
-
-def player_rank_up(pts):
-    return "Well done! You have advanced to the qualifying stage. Win 2 out of your next 3 games to rank up." if pts >= 100 else False
