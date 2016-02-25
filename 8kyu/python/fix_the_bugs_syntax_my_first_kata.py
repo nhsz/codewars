3ea0fed4bb6f239e8bf15cfc35218076ac9ca2f8
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:22:11 -0300

    Delete fix_the_bugs_syntax_my_first_kata.py

--- 8kyu/python/fix_the_bugs_syntax_my_first_kata.py
+++
@@ -1,8 +0,0 @@
-# http://www.codewars.com/kata/56aed32a154d33a1f3000018/
-
-
-def my_first_kata(a, b):
-    if type(a) == int and type(b) == int:
-        return a % b + b % a
-    else:
-        return False
