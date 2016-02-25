1d2b25e8df4a58d666bf556bf193c79f9ab1b698
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:23:22 -0300

    Delete get_the_middle_character.py

--- 8kyu/python/get_the_middle_character.py
+++
@@ -1,8 +0,0 @@
-# http://www.codewars.com/kata/56747fd5cb988479af000028/
-
-
-def get_middle(s):
-    if len(s) % 2 == 1:
-        return s[len(s) / 2]
-    else:
-        return s[len(s) / 2 - 1 : len(s) / 2 + 1]
