3a02d04192b4fdee80dba056a107bf7554aa05a9
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 16:14:58 -0300

    Create merged_string_checker.py

---
+++ 5kyu/merged_string_checker.py
@@ -0,0 +1,14 @@
+# http://www.codewars.com/kata/54c9fcad28ec4c6e680011aa/
+
+def is_merge(s, part1, part2):
+    if not part1:
+        return s == part2
+    if not part2:
+        return s == part1
+    if not s:
+        return part1 + part2 == ''
+    if s[0] == part1[0] and is_merge(s[1:], part1[1:], part2):
+        return True
+    if s[0] == part2[0] and is_merge(s[1:], part1, part2[1:]):
+        return True
+    return False
