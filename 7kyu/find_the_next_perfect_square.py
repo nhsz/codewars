2fa11d2b21450db072c3dea22e9d4d28b8ba4fa1
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 17:04:02 -0300

    Create find_the_next_perfect_square.py

---
+++ 7kyu/find_the_next_perfect_square.py
@@ -0,0 +1,19 @@
+# http://www.codewars.com/kata/56269eb78ad2e4ced1000013/
+
+import math
+    
+
+def is_perfect_square(n):
+    return (math.sqrt(n)).is_integer()
+        
+    
+def find_next_square(sq):
+    if not is_perfect_square(sq):
+        return -1
+    else:
+        next_perfect_square = sq + 1
+        while True:
+            if is_perfect_square(next_perfect_square):
+                return next_perfect_square
+            else:
+                next_perfect_square += 1
