bc99d6d46f24c21dbed15968b5f2f19c88877a76
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:29:30 -0300

    Delete reverse_list_order.py

--- 8kyu/python/reverse_list_order.py
+++
@@ -1,9 +0,0 @@
-# http://www.codewars.com/kata/53da6d8d112bd1a0dc00008b/
-
-
-def reverse_list(l):
-    reversed_list = []
-    for i in reversed(l):
-        reversed_list.append(i)
-
-    return reversed_list
