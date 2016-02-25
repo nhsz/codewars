8030cf01584fb12e42cbd0b9138d48e9adc7cf64
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-25 00:34:09 -0300

    Update add_length.py

--- 8kyu/add_length.py
+++ 8kyu/add_length.py
@@ -1,6 +1,5 @@
 # http://www.codewars.com/kata/559d2284b5bb6799e9000047/
 
-
 def add_length(str_):
     list = str_.split()
     new_list = []
@@ -8,5 +7,4 @@ def add_length(str_):
     for word in list:
         new_word = word + " " + str(len(word))
         new_list.append(new_word)
-
     return new_list
