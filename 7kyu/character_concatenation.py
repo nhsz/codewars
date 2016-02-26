08da884c71e32c78926b720894e3b3775857a72c
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-26 02:05:38 -0300

    Update character_concatenation.py

--- 7kyu/character_concatenation.py
+++ 7kyu/character_concatenation.py
@@ -1,12 +1,13 @@
 # http://www.codewars.com/kata/55147ff29cd40b43c600058b/
 
 def char_concat(word):
-    middle = len(word) / 2
-    if len(word) % 2 == 1:
-        word = word[0:middle] + word[middle + 1:len(word)]
+    n = len(word)
+    middle = n / 2
+    if n % 2 == 1:
+        word = word[0:middle] + word[middle + 1:n]
     new_word = ""
     i = 0
-    j = len(word) - 1
+    j = n - 1
     while i < middle and j > middle - 1:
         new_word += word[i] + word[j] + str(i + 1)
         i += 1
