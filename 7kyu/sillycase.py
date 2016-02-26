66cf2a693df6bfe91f937133034f45456dc8e32b
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-26 02:19:36 -0300

    Update sillycase.py

--- 7kyu/sillycase.py
+++ 7kyu/sillycase.py
@@ -1,8 +1,5 @@
 # http://www.codewars.com/kata/552ab0a4db0236ff1a00017a/
 
 def sillycase(silly):
-    n = len(silly)
-    middle = n / 2
-    if n % 2 == 1:
-        middle += 1
-    return silly[0:middle].lower() + silly[middle:n].upper()
+    middle = (len(silly) + 1) / 2
+    return silly[:middle].lower() + silly[middle:].upper()
