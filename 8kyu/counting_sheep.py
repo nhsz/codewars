2077adac993917e0ff689f6fc9b23d65d9f758a3
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 14:23:10 -0300

    Update counting_sheep.py

--- 8kyu/counting_sheep.py
+++ 8kyu/counting_sheep.py
@@ -1,8 +1,8 @@
 # http://www.codewars.com/kata/54edbc7200b811e956000556
 
-def count_sheeps(arrayOfSheeps):
-    counterOfPresentSheeps = 0
-    for sheep in arrayOfSheeps:
-        if(sheep):
-            counterOfPresentSheeps += 1
-    return counterOfPresentSheeps
+def count_sheeps(array_of_sheeps):
+    counter_of_present_sheeps = 0
+    for sheep in array_of_sheeps:
+        if sheep:
+            counter_of_present_sheeps += 1
+    return counter_of_present_sheeps
