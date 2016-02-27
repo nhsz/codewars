13116560a363b2fcf8def7943d68a2af86db3dbb
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 12:25:36 -0300

    Update largest_5_digit_number_in_a_series.py

--- 5kyu/largest_5_digit_number_in_a_series.py
+++ 5kyu/largest_5_digit_number_in_a_series.py
@@ -9,6 +9,6 @@ def solution(digits):
         current_number = digits[start:end + 1]
         if current_number > largest_number_found:
             largest_number_found = current_number
-        start += 1 
-        end += 1 
+        start += 1
+        end += 1
     return int(largest_number_found)
