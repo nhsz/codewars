f1644ffef07811810ac99b07da682f9d2dcb2d71
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 15:47:26 -0300

    Update the_if_function.py

--- 8kyu/the_if_function.py
+++ 8kyu/the_if_function.py
@@ -1,4 +1,4 @@
 # http://www.codewars.com/kata/the-if-function/
 
-def _if(bool, func1, func2):
-    return func1() if bool else func2()
+def _if(boolean_value, func1, func2):
+    return func1() if boolean_value else func2()
