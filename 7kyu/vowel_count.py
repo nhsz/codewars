127e2ab7ab919ed645432e23fecb9225295d78e6
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 16:21:08 -0300

    Update vowel_count.py

--- 7kyu/vowel_count.py
+++ 7kyu/vowel_count.py
@@ -1,8 +1,8 @@
 # http://www.codewars.com/kata/54ff3102c1bad923760001f3/
 
-def getCount(inputStr):
+def getCount(input_string):
     num_vowels = 0
-    for letter in inputStr:
+    for letter in input_string:
         if letter in {'a', 'e', 'i', 'o', 'u'}:
             num_vowels += 1
     return num_vowels
