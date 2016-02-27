114704d543b400f49082f384ef349129c741f16d
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 13:32:39 -0300

    Update barking_mad.py

--- 8kyu/barking_mad.py
+++ 8kyu/barking_mad.py
@@ -1,13 +1,12 @@
 # http://www.codewars.com/kata/54dba07f03e88a4cec000caf/
 
-class Dog ():
+class Dog(object):
     def __init__(self, breed):
         self.breed = breed
 
     def bark(self):
         return "Woof"
-    
+
 
 snoopy = Dog("Beagle")
-snoopy.bark = lambda: "Woof"
 scoobydoo = Dog("Great Dane")
