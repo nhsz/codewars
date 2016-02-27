dc3a3424869d268a33ba1066a246f73bac03724c
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 14:30:51 -0300

    Update fix_the_bugs_syntax_my_first_kata.py

--- 8kyu/fix_the_bugs_syntax_my_first_kata.py
+++ 8kyu/fix_the_bugs_syntax_my_first_kata.py
@@ -1,7 +1,7 @@
 # http://www.codewars.com/kata/56aed32a154d33a1f3000018/
 
 def my_first_kata(a, b):
-    if type(a) == int and type(b) == int:
+    if isinstance(a, int) and isinstance(b, int):
         return a % b + b % a
     else:
         return False
