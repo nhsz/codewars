938d1c815d3fb01c7deb3507cba2b0e7a2ac51f2
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 14:20:46 -0300

    Update convert_boolean_values_to_strings_yes_or_no.py

--- 8kyu/convert_boolean_values_to_strings_yes_or_no.py
+++ 8kyu/convert_boolean_values_to_strings_yes_or_no.py
@@ -1,4 +1,4 @@
 # http://www.codewars.com/kata/53369039d7ab3ac506000467/
 
-def bool_to_word(bool):
-    return "Yes" if bool else "No"
+def bool_to_word(boolean_value):
+    return "Yes" if boolean_value else "No"
