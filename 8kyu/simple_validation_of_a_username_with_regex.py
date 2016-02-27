40ae84bd126da9d7a5de3a6fcee03854d1656a81
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 15:38:26 -0300

    Update simple_validation_of_a_username_with_regex.py

--- 8kyu/simple_validation_of_a_username_with_regex.py
+++ 8kyu/simple_validation_of_a_username_with_regex.py
@@ -1,15 +1,15 @@
 # http://www.codewars.com/kata/56a3f08aa9a6cc9b75000023/
 
-def allowed_username_length(username):
-    return len(username) >= 4 and len(username) <= 16
-    
+def allowed_name_length(name):
+    return len(name) >= 4 and len(name) <= 16
 
-def allowed_username_characters(username):
+
+def allowed_name_characters(name):
     valid_char = True
-    for char in username:
+    for char in name:
         valid_char = valid_char and (char.islower() or char.isdigit() or char == '_')
     return valid_char
 
 
-def validate_usr(username):
-    return allowed_username_length(username) and allowed_username_characters(username)
+def validate_usr(name):
+    return allowed_name_length(name) and allowed_name_characters(name)
