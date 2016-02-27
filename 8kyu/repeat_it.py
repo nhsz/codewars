e22e576aa46a9c8ca95642a287f66ef3c394627c
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 15:35:20 -0300

    Update repeat_it.py

--- 8kyu/repeat_it.py
+++ 8kyu/repeat_it.py
@@ -1,4 +1,4 @@
 # http://www.codewars.com/kata/557af9418895e44de7000053/
 
 def repeat_it(string, n):
-    return string * n if type(string) == str else "Not a string"
+    return string * n if isinstance(string, str) else "Not a string"
