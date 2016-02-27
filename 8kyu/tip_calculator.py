931e28c64f5933ad518ab014a9a919ca31e3f1ec
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 15:50:32 -0300

    Update tip_calculator.py

--- 8kyu/tip_calculator.py
+++ 8kyu/tip_calculator.py
@@ -4,8 +4,14 @@ import math
 
 
 def calculate_tip(amount, rating):
-    tip_percentage = {"terrible": 0, "poor": 0.05, "good": 0.1, "great": 0.15, "excellent": 0.2}
-    
+    tip_percentage = {
+        "terrible": 0,
+        "poor": 0.05,
+        "good": 0.1,
+        "great": 0.15,
+        "excellent": 0.2
+    }
+
     if rating.lower() in tip_percentage.keys():
         return math.ceil(amount * tip_percentage[rating.lower()])
     else:
