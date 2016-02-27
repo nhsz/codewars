2022e6d066df109565695e93b666965d2733dff8
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 15:34:16 -0300

    Update playing_with_cubes_2.py

--- 8kyu/playing_with_cubes_2.py
+++ 8kyu/playing_with_cubes_2.py
@@ -1,16 +1,14 @@
 # http://www.codewars.com/kata/55c0ac142326fdf18d0000af/
 
-class Cube:
+class Cube(object):
     def __init__(self, side=0):
         """Construct the Cube"""
         self._side = abs(side)
 
-
     def get_side(self):
         """Return the side of the Cube"""
         return self._side
-  
-  
+
     def set_side(self, new_side):
         """Set the value of the Cube's side."""
         self._side = abs(new_side)
