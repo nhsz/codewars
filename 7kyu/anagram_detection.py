c36953e4e10d10e491caa918e90f7c10ea0bf808
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 13:19:54 -0300

    Update anagram_detection.py

--- 7kyu/anagram_detection.py
+++ 7kyu/anagram_detection.py
@@ -1,4 +1,4 @@
 # http://www.codewars.com/kata/529eef7a9194e0cbc1000255/
 
 def is_anagram(test, original):
-    return sorted(original.lower()) == sorted(test.lower()) 
+    return sorted(original.lower()) == sorted(test.lower())
