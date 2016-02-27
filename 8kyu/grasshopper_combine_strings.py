336018b7a563a25940252ed1fd83c7267f35c24b
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-02-27 12:35:16 -0300

    Update grasshopper_combine_strings.py

--- 8kyu/grasshopper_combine_strings.py
+++ 8kyu/grasshopper_combine_strings.py
@@ -1,4 +1,4 @@
 # http://www.codewars.com/kata/55f73f66d160f1f1db000059/
 
 def combine_names(first_name, last_name):
-    return first name + " " + last_name
+    return "{0} {1}".format(first_name, last_name)
