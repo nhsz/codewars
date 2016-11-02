b6cc1f5d8c966738d65f567c1fe85385770e3822
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-11-02 14:38:55 -0300

    Update trainingJS4.js

--- 8kyu/trainingJS4.js
+++ 8kyu/trainingJS4.js
@@ -1,29 +1,31 @@
 // https://www.codewars.com/kata/training-js-number-4-basic-data-types-array/
 
 function getLength(arr) {
-  //return length of arr
+  // return length of arr
   return arr.length;
 }
 
 function getFirst(arr) {
-  //return the first element of arr
+  // return the first element of arr
   return arr[0];
 }
 
 function getLast(arr) {
-  //return the last element of arr
+  // return the last element of arr
   return arr[arr.length - 1];
 }
 
 function pushElement(arr) {
   var el = 1;
-  //push el to arr
+  // push el to arr
+  arr.push(el);
   
-  return arr.push(el);
+  return arr;
 }
 
 function popElement(arr) {
-  //pop an element from arr
+  // pop an element from arr
+  arr.pop();
   
-  return arr.pop();
+  return arr;
 }
