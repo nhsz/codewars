bf3586f039e355f0d2a40b05b2a8307da0ccdca6
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-11-02 11:56:54 -0300

    Create trainingJS3.js

---
+++ trainingJS3.js
@@ -0,0 +1,45 @@
+// https://www.codewars.com/kata/training-js-number-3-basic-data-types-string/
+
+var a1 = "A";
+var a2 = "a";
+var b1 = "B";
+var b2 = "b";
+var c1 = "C";
+var c2 = "c";
+var d1 = "D";
+var d2 = "d";
+var e1 = "E";
+var e2 = "e";
+var n1 = "N";
+var n2 = "n";
+
+function Dad() {
+  // select some variable to combine "Dad"
+  return d1 + a2 + d2;
+}
+
+function Bee() {
+  // select some variable to combine "Bee"
+  return b1 + e2 + e2;
+}
+
+function banana() {
+  // select some variable to combine "banana"
+  return b2 + a2 + n2 + a2 + n2 + a2;
+}
+
+//answer some questions if you finished works above
+function answer1() {
+  // the answer should be "yes" or "no"
+  return "yes";
+}
+
+function answer2() {
+  // the answer should be "yes" or "no"
+  return "no";
+}
+
+function answer3() {
+  // the answer should be "yes" or "no"
+  return "yes";
+}
