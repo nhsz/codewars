dd3c1ca4ccd8511c67ff945b5da55971d61edbe9
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-11-02 10:16:53 -0300

    Create trainingJS2.js

---
+++ 8kyu/trainingJS2.js
@@ -0,0 +1,38 @@
+// https://www.codewars.com/kata/training-js-number-2-basic-data-types-number/
+
+var v1 = 50;
+var v2 = 100;
+var v3 = 150;
+var v4 = 200;
+var v5 = 2;
+var v6 = 250;
+
+function equal1() {
+  var a = v1   
+  var b = v1   
+  return a + b;
+}
+
+function equal2() {
+  var a = v3;
+  var b = v1;
+  return a - b;
+}
+
+function equal3(){
+  var a = v1;
+  var b = v5;
+  return a * b;
+}
+
+function equal4() {
+  var a = v4;
+  var b = v5;
+  return a / b;
+}
+
+function equal5(){
+  var a = v6;
+  var b = v3;
+  return a % b;
+}
