81fe58043641a928f12038e127f418d707a53c86
Nicolás Quiroz <nhquiroz@users.noreply.github.com>
2016-11-02 16:51:10 -0300

    Create trainingJS6.js

---
+++ 8kyu/trainingJS6.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/training-js-number-6-basic-data-types-boolean-and-conditional-statements-if-dot-else/
+
+function trueOrFalse(val){
+  return Boolean(val).toString();
+}
